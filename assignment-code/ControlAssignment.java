0386568771e6f6df3b72c62ce6501b095e855e41
Edison Zhuang <edisen.zhuang@gmail.com>
2023-01-27 09:09:52 +0800

    convert snake_case to camelCase convention

--- assignment-code/ControlAssignment.java
+++ assignment-code/ControlAssignment.java
@@ -1,7 +1,7 @@
 public class ControlAssignment {
     public static void main (String args[]){
         // 1. Determine if a number is divisible by 3, 5 or both.
-        int num_to_test = 995;
+        int numToTest = 995;
         /* 2. Given a sample budget, determine what to ride given the following conditions:
             - if budget is greater than 100, ride a taxi
             - if budget is between 50 to 100, ride train
@@ -9,6 +9,6 @@ public class ControlAssignment {
             - if budget is between 0 to 19, walk */
         float budget = 72.7f;
         // 3. Use a switch case to determine the zodiac animal given a number from 1 to 12. (1 - rat, 2 - ox, 3 - tiger, etc.)
-        int animal_number = 7;
+        int animalNumber = 7;
     }
 }
